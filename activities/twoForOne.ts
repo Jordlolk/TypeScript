c0f746b6a4e9818f279cd3fd9f54fa6865f61532
jordlolk <jordeallefsilvadecarvalho@gmail.com>
2024-10-17 09:20:57 -0300

    fix: clean null lines

---
+++ activities/twoForOne.ts
@@ -0,0 +1,17 @@
+/* In english when we have to make an 
+  offer to buy one thing and receive another for free. 
+  I say two-for-one, but in this case we have a special case. 
+  i´ve to create a function that says "one for you, one for me."
+  but if I know the name of the person i migth to say "one for Alice, one for me"
+  if(isBlack == 0 && howManyZero === 2){
+      return `${colors.indexOf(color[0])} kiloohms`
+    }else{
+      return code*zeros+' ohms'
+    }
+*/
+function twoForOne(personName: any) {
+  let personExists = typeof personName != "string" ? "you" : personName;
+  console.log(`One for ${personExists}, one for me.`);
+}
+twoForOne("Alice");
+twoForOne(undefined);
